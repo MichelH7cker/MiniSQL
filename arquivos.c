13d9252e6de2ae2b583c202aa6890b69a82a982d
MichelH4cker <84712244+MichelH4cker@users.noreply.github.com>
2021-07-25 10:40:54 -0300

    Add files via upload

--- arquivos.c
+++ arquivos.c
@@ -36,9 +36,7 @@ void imprimeColuna(int colunaEntrada, char **arquivo){
     int idColunaInicial  = colunaEntrada;  //numero de qual coluna é
     int coluna = idColunaInicial;
     do{  
-        cursor = fgetc(pAarquivo);
-        //printf("%c", cursor);
-        
+        cursor = fgetc(pAarquivo);        
         if (acumuladorTabs == coluna && acumuladorTabs >= tamanhoArquivo){
             printf("%c", cursor);
         }
@@ -60,16 +58,11 @@ void imprimeColuna(int colunaEntrada, char **arquivo){
 }
 
 int escolheColunas(char **conteudoSelect, int tamanhoConteudoSelect, char **arquivo){
-    //printf("O arquivo está armazenando: %s\n", arquivo[0]);
-    
     char cabecalhoProgs[7][100] = {{"Progs.Instituicao"}, {"Progs.Programa"}, {"Progs.Nivel"}, {"Progs.Sigla"}, {"Progs.TemDoutorado"}, {"Progs.Nome"}, {"Progs.AreadeAvaliacao"}};
     char cabecalhoDocentes[6][100] = {{"Docentes.AnodaTitulacao"}, {"Docentes.CodigoPPG"}, {"Docentes.Nacionalidade"}, {"Docentes.Nome"}, {"Docentes.PaisdaInstituicao"}, {"Docentes.Sexo"}};
     char cabecalhoTrabalhos[5][100] = {{"Trabalhos.Ano"}, {"Trabalhos.Autor"}, {"Trabalhos.CodigoPPG"}, {"Trabalhos.Idioma"}, {"Trabalhos.Orientador"}};
-    
-    //printf("o cabecalhoProgs na posicao 0 vale: %s\n", cabecalhoProgs[0]);
-    
+        
     for (int posicaoConteudoSelect = 0; posicaoConteudoSelect < tamanhoConteudoSelect; posicaoConteudoSelect++){
-        //printf("o conteudo select é: %s\n", conteudoSelect[posicaoConteudoSelect]);
         if (strcmp(arquivo[0], "Docentes") == 0){
             for (int indice = 0; indice < 5; indice++){ //FOR DO DICIONÁRIO
                 if ((strcmp(conteudoSelect[posicaoConteudoSelect], cabecalhoDocentes[indice])) == 0){
@@ -79,10 +72,7 @@ int escolheColunas(char **conteudoSelect, int tamanhoConteudoSelect, char **arqu
             }
             
         } else if ((strcmp(arquivo[0], "Progs") == 0)){
-            //printf("entrei no else if\n");
             for (int indice = 0; indice < 7; indice++){
-                //printf("O conteudo do select e: %s\n", conteudoSelect[posicaoConteudoSelect]);
-                //printf("O cabecalhoProgs %s\n", cabecalhoProgs[indice]);
                 if ((strcmp(conteudoSelect[posicaoConteudoSelect], cabecalhoProgs[indice])) == 0){
                     return indice;
                 } 
@@ -104,19 +94,12 @@ void selecionaArquivos(char **conteudoFrom, int tamanhoConteudoFrom, char **arqu
     for (int i = 0; i < tamanhoConteudoFrom; i++){
         printf("\n");
         if(strcmp(conteudoFrom[i], "Docentes") == 0){
-            //printf("selecionando arquivo Docentes\n");
-            //arquivo = "Progs";
             strcpy(arquivo[0], "Docentes");
-            //printf("A variavel arquivo armazenou: %s\n", arquivo[0]);
 
         } else if (strcmp(conteudoFrom[i], "Progs") == 0){
-            //printf("selecionando arquivo Progs\n");
-            //arquivo = "Progs";
             strcpy(arquivo[0], "Progs");
 
         } else if (strcmp(conteudoFrom[i], "Trabalhos") == 0){
-            //printf("selecionando arquivo Trabalhos \n");
-            //arquivo = "Trabalhos";
             strcpy(arquivo[0], "Trabalhos");
 
         } else { //quando não tem mais arquivos
