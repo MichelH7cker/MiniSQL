132df934a4357b1a62b8b3de80f045dad471fa68
MichelH4cker <84712244+MichelH4cker@users.noreply.github.com>
2021-07-26 15:44:59 -0300

    Add files via upload
    Projeto quase finalizado e muitos bugs foram consertados.
    O que falta fazer:
    * enteder melhor como fazer a ordem da saída
    * comparações booleanas entre as pesquisas

--- arquivos.c
+++ arquivos.c
@@ -8,100 +8,48 @@
 #define COLUNASDOCENTES 6    // QUANTIDADE DE COLUNAS DE CADA ARQUIVO
 #define COLUNASTRABALHOS 5   //
 
-int encontraArquivo(char **arquivo) {
-    if (strcmp(arquivo[0], "Docentes") == 0){
-        return COLUNASDOCENTES;
-    }
-    if (strcmp(arquivo[0], "Progs") == 0) {
-        return COLUNASPROGS;
-    }
-    return COLUNASTRABALHOS;
-}
+#define DOCENTES 1           //        
+#define PROGS 2              // ID PARA CADA ARQUIVO
+#define TRABALHOS 3          //
 
-void imprimeSaida(int *colunaEntrada, char **arquivo){
-    FILE *pArquivo;
-    char cursor;
-    int tamanhoArquivo = encontraArquivo(arquivo);
+#define LINHASAIDA 101       // LINHAS TOTAIS DA MATRIZ DE SAÍDA
 
-    if (tamanhoArquivo == COLUNASPROGS) {
-        pArquivo = fopen("Progs.tsv","r");
-    }
-    else if (tamanhoArquivo == COLUNASTRABALHOS) {
-        pArquivo = fopen("Trabalhos.tsv","r");
-    }
-    else {
-        pArquivo = fopen("Docentes.tsv","r");
-    }
-    
-    if (pArquivo == NULL){
-        printf("Erro ao tentar abrir o arquivo!");
-        exit(1);
-    }
-
-    int acumuladorTabs   = 0;
-    int acumuladorLinhas = 0;
-    int idColunaInicial  = *colunaEntrada;  //numero de qual coluna é
-    int coluna = idColunaInicial;
-    do{  
-        cursor = fgetc(pArquivo);        
-        if (acumuladorTabs == coluna && acumuladorTabs >= tamanhoArquivo){
-            printf("%c", cursor);
-        }
-        if (cursor == '\t'){
-            acumuladorTabs++;
-        }
-        
-        if (cursor == '\n'){
-            acumuladorTabs++;
-            coluna += tamanhoArquivo;
-            if (idColunaInicial < tamanhoArquivo - 1 && acumuladorTabs > tamanhoArquivo) {
-                printf("\n"); 
-            }          //serve pra quase todas as colunas, menos a ultima (da um enter a mais do que precisa)
-        }
-        
-    } while (cursor != EOF); //enquanto não for final de arquivo 
-
-    fclose(pArquivo);
-}
-
-void percorreCabecalho(char cabecalho[][100], char **conteudoSelect, int tamanhoConteudoSelect, int **colunaSelecionada, int **selectAtual){
-    for (int indiceCabecalho = 0; indiceCabecalho < COLUNASDOCENTES; indiceCabecalho++){
-        if (strcmp(conteudoSelect[**selectAtual], cabecalho[indiceCabecalho]) == 0){
-            **colunaSelecionada = indiceCabecalho;
-        }
-    }
-}
-
-void escolheColunas(char **conteudoSelect, int tamanhoConteudoSelect, char **arquivo, int *colunaSelecionada, int *selectAtual){
+int percorreCabecalho(char **conteudoSelect, int tamanhoConteudoSelect, char **arquivo, int selectAtual, int **colunaSelecionada){
     char cabecalhoProgs[COLUNASPROGS][100] = {{"Progs.Instituicao"}, {"Progs.Programa"}, {"Progs.Nivel"}, {"Progs.Sigla"}, {"Progs.TemDoutorado"}, {"Progs.Nome"}, {"Progs.AreadeAvaliacao"}};
     char cabecalhoDocentes[COLUNASDOCENTES][100] = {{"Docentes.AnodaTitulacao"}, {"Docentes.CodigoPPG"}, {"Docentes.Nacionalidade"}, {"Docentes.Nome"}, {"Docentes.PaisdaInstituicao"}, {"Docentes.Sexo"}};
-    char cabecalhoTrabalhos[COLUNASTRABALHOS][100] = {{"Trabalhos.Ano"}, {"Trabalhos.Autor"}, {"Trabalhos.CodigoPPG"}, {"Trabalhos.Idioma"}, {"Trabalhos.Orientador"}};    
-
-    if ((strcmp(arquivo[0], "Docentes")) == 0){
-        percorreCabecalho(cabecalhoDocentes, conteudoSelect, tamanhoConteudoSelect, &colunaSelecionada, &selectAtual); 
+    char cabecalhoTrabalhos[COLUNASTRABALHOS][100] = {{"Trabalhos.Ano"}, {"Trabalhos.Autor"}, {"Trabalhos.CodigoPPG"}, {"Trabalhos.Idioma"}, {"Trabalhos.Orientador"}};
     
-    } else if (strcmp(arquivo[0], "Progs") == 0){
-        percorreCabecalho(cabecalhoProgs, conteudoSelect, tamanhoConteudoSelect, &colunaSelecionada, &selectAtual);
+    for (int i = 0; i < COLUNASDOCENTES; i++){
+        if (strcmp(conteudoSelect[selectAtual], cabecalhoDocentes[i]) == 0){
+            **colunaSelecionada = i;
+            return DOCENTES;
+        }
+    }
+    for (int j = 0; j < COLUNASPROGS; j++){
+        if (strcmp(conteudoSelect[selectAtual], cabecalhoProgs[j]) == 0){
+            **colunaSelecionada = j;
+            return PROGS;
+        }
+    }
+    for (int k = 0; k < COLUNASTRABALHOS; k++){
+        if (strcmp(conteudoSelect[selectAtual], cabecalhoTrabalhos[k]) == 0){
+            **colunaSelecionada = k;
+            return TRABALHOS;
+        }
+    }
+}
 
+void selecionaArquivoColuna(char **conteudoSelect, int tamanhoConteudoSelect, char **arquivo, int posicaoSelect, int *colunaSelecionada){
+    int arquivoSelecionado = percorreCabecalho(conteudoSelect , tamanhoConteudoSelect, arquivo, posicaoSelect, &colunaSelecionada);
+    
+    if (arquivoSelecionado == DOCENTES){
+        strcpy(arquivo[0], "Docentes");
+    } else if(arquivoSelecionado == PROGS){
+        strcpy(arquivo[0], "Progs");
     } else {
-        percorreCabecalho(cabecalhoTrabalhos, conteudoSelect, tamanhoConteudoSelect, &colunaSelecionada, &selectAtual);
+        strcpy(arquivo[0], "Trabalhos");
     }
     
 }
 
-void selecionaArquivos(char **conteudoFrom, int tamanhoConteudoFrom, char **arquivo, int posicaoFrom){
-    printf("\n");
-    if(strcmp(conteudoFrom[posicaoFrom], "Docentes") == 0){
-        strcpy(arquivo[0], "Docentes");
-
-    } else if (strcmp(conteudoFrom[posicaoFrom], "Progs") == 0){
-        strcpy(arquivo[0], "Progs");
-
-    } else if (strcmp(conteudoFrom[posicaoFrom], "Trabalhos") == 0){
-        strcpy(arquivo[0], "Trabalhos");
-
-    } else { //quando não tem mais arquivos
-        strcpy(arquivo[0], "none");
-    } 
-}
-
+//select Trabalhos.CodigoPPG, Docentes.PaisdaInstituicao, Progs.AreadeAvaliacao from Trabalhos, Docentes, Progs
