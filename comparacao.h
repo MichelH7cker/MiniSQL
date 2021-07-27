5e68cbee8ca18031d66795702896968be2c26880
GuilhermeAMoreira <85847789+GuilhermeAMoreira@users.noreply.github.com>
2021-07-27 13:59:37 -0300

    Update comparacao.h

--- comparacao.h
+++ comparacao.h
@@ -1,5 +1,13 @@
-#ifndef __ARQUIVOS_H__          // entre arquivos
-#define __ARQUIVOS_H__          // impede loop
+#ifndef __COMPARACAO_H__          // entre arquivos
+#define __COMPARACAO_H__          // impede loop
+
+/**
+ * 
+ * @param comandoWhere comando passado pela entrada na parte where
+ * @param tamanhoComandoWhere quantidade de palavra contidas no comandoWhere
+ * @param whereFiltrado contém apenas as colunas a serem comparadas
+ */
+void filtraComandoWhere(char **comandoWhere, int tamanhoComandoWhere, char **whereFiltrado);
 
 /**
  * @param conteudoWhere quais são as colunas a serem comparadas no where
@@ -11,10 +19,17 @@ int numdeIguais(char **conteudoWhere, int tamanhoConteudoWhere);
 
 /**
  * 
- * 
  * @param conteudoWhere quais são as colunas a serem comparadas no where
  * @param tamanhoConteudoWhere quantas palavras há no where
  */
-void comparacao(char **conteudoWhere, int tamanhoConteudoWhere);
+void comparaDados(int *linhasIguaisRes, char **conteudoWhere, int tamanhoConteudoWhere);
+
+/**
+ * verifica quais linhas podem ser imprimidas
+ * @param linhasIguaisRes lista de linhas que podem ser imprimidas
+ * @param comandoWhere comando passado pela entrada na parte where
+ * @param tamanhoComandoWhere quantidade de palavra contidas no comandoWhere
+ */
+void verificaLinhasPermitidas(int *linhasIguaisRes, char **comandoWhere, int tamanhoComandoWhere);
 
 #endif
